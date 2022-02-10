5d185198e07add12b6121b1f6cb89aeae838a40e
tider <tider@quansitech.com>
2022-02-10 15:32:34 +0800

    t

--- esm/components/Area/index.d.ts
+++
@@ -1,7 +0,0 @@
-import './index.less';
-export declare const Area: {
-    (props: any): any;
-    defaultProps: {
-        url: string;
-    };
-};
--- esm/components/Form/index.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { FormProps as FomilyFormProps } from '@formily/antd';
-export declare type FormLayoutType = 'vertical' | 'horizontal';
-export interface FormProps extends FomilyFormProps {
-    formLayout?: FormLayoutType;
-}
-export declare const Form: React.FC<FormProps>;
-export default Form;
